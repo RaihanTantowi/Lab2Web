042dd70fe25dac056d1d5a1d35df8d42c7fc243a
RaihanTantowi <raihantantowi9@gmail.com>
2023-03-25 09:06:19 +0700

    Update file

---
+++ lab2_php_dasar/formInput&Output.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Input & Output PHP</title>
+</head>
+<body>
+	<h2>Form Input & Output PHP</h2>
+	<form method="POST" action="">
+		<label for="nama">Nama:</label>
+		<input type="text" name="nama"><br><br>
+		<label for="tgl_lahir">Tanggal Lahir:</label>
+		<input type="date" name="tgl_lahir"><br><br>
+		<label for="pekerjaan">Pekerjaan:</label>
+		<select name="pekerjaan">
+			<option value="">--Pilih Pekerjaan--</option>
+			<option value="Polisi">Polisi</option>
+			<option value="Programmer">Programmer</option>
+			<option value="Guru">Guru</option>
+		</select><br><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+	<br>
+	<?php
+		if(isset($_POST['submit'])){
+			$nama = $_POST['nama'];
+			$tgl_lahir = $_POST['tgl_lahir'];
+			$pekerjaan = $_POST['pekerjaan'];
+			
+			// Menghitung umur berdasarkan tanggal lahir
+			$umur = date_diff(date_create($tgl_lahir), date_create('now'))->y;
+			
+			// Menentukan gaji berdasarkan pekerjaan
+			switch($pekerjaan){
+				case 'Polisi':
+					$gaji = 8000000;
+					break;
+				case 'Programmer':
+					$gaji = 5000000;
+					break;
+				case 'Guru':
+					$gaji = 4000000;
+					break;
+				default:
+					$gaji = 0;
+			}
+			
+			// Menampilkan hasil output
+			echo "Nama: ".$nama."<br>";
+			echo "Umur: ".$umur." tahun<br>";
+			echo "Pekerjaan: ".$pekerjaan."<br>";
+			echo "Gaji: Rp ".number_format($gaji,0,",",".")."<br>";
+		}
+	?>
+</body>
+</html>
